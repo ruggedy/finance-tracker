0ca25675f09fb5814ae48e1a2410efebf7182836
ruggedy <shout2john@yahoo.com>
2016-04-12 00:46:14 +0100

    fully functional app .. cool stuff

---
+++ app/models/stock.rb
@@ -0,0 +1,26 @@
+class Stock < ActiveRecord::Base
+has_many :user_stocks
+has_many :users, through: :user_stocks
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+
+  def self.new_from_lookup(ticker_symbol)
+    lookup_up_quote = StockQuote::Stock.quote(ticker_symbol)
+    return nil unless lookup_up_quote.name
+
+    new_stock = new(ticker: lookup_up_quote.symbol, name: lookup_up_quote.name)
+    new_stock.last_price = new_stock.price
+    new_stock
+  end
+
+  def price
+
+    closing_price = StockQuote::Stock.quote(ticker).close
+    return "#{closing_price} (closing)" if closing_price
+
+    opening_price = StockQuote::Stock.quote(ticker).open
+    return "#{opening_price} (opening)" if opening_price
+    'unavailable'
+  end
+end
